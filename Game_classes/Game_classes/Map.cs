4f4e9857126837fac50bb2b7752446a96a7ee62c
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-06 13:49:39 -0500

    Cretaed a method OnMap that returns a bool - checks to see if points are within the bounds of the map

--- Game_classes/Game_classes/Map.cs
+++ Game_classes/Game_classes/Map.cs
@@ -19,5 +19,12 @@ namespace Game_classes
             Width = width;
             Height = height;
         }
+
+        public bool OnMap(Point point)
+        {
+            bool inBounds = point.X >= 0 &&  point.X < Width && point.Y >=0 && point.Y < Height;
+
+            return inBounds;
+        }
     }
 }
