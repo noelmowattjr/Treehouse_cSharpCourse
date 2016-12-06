b5de3551443750c21542412bd76d92d450d66708
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-05 19:58:38 -0500

    Demonstrating the diff between public and private access modifiers

--- Game_classes/Game_classes/Program.cs
+++ Game_classes/Game_classes/Program.cs
@@ -11,6 +11,16 @@ namespace Game_classes
         static void Main(string[] args)
         {
             Tower tower = new Tower();  //--new tower instance aka new tower object
+
+            Map map = new Map();    //--map object
+
+            /*  Demonstrating the difference between "public" and "private" access modifiers    */
+            //--Access Width & Height field in Map class - then assign both a value
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
+            Console.WriteLine("The area is: {0}", area);
         }
     }
 }
