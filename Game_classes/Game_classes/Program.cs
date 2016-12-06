b3daa5146a7b9b60cb4c0d739604291acd8e14f5
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-05 19:34:34 -0500

    Setting up workspace!
    Project setup

---
+++ Game_classes/Game_classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tower tower = new Tower();  //--new tower instance aka new tower object
+        }
+    }
+}
