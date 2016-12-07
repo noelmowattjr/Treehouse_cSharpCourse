bb18681b1e5fc6239e919e765a3f96852f5660c0
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-06 19:43:26 -0500

    Testing out the use of a bool condition stored in a variable

--- Game_classes/Game_classes/Program.cs
+++ Game_classes/Game_classes/Program.cs
@@ -16,6 +16,44 @@ namespace Game_classes
             
             int area = map.Width * map.Height;
             Console.WriteLine("The area is: {0}", area);
+
+            //--Testing concept i just learned
+            Console.Write("Enter your age: ");
+            string entry = Console.ReadLine();
+            int age;
+
+            while (!(int.TryParse(entry, out age)))
+            {
+                Console.Write("Invalid! Enter your age: ");
+                entry = Console.ReadLine();
+            }
+
+            Console.Write("Enter your friend's age: ");
+            string entry2 = Console.ReadLine();
+            int age2;
+
+            while (!(int.TryParse(entry2, out age2)))
+            {
+                Console.Write("Invalid! Enter your age: ");
+                entry2 = Console.ReadLine();
+            }
+
+            //--Call function
+            var decision = IsOldEnough(age, age2);
+
+            if (decision == true)
+                Console.WriteLine("You may enter the establishment!");
+
+            if (decision != true)
+                Console.WriteLine("You aren't old enough! Sorry.");
+
+        }
+
+        public static bool IsOldEnough(int Age, int Age2)
+        {
+            bool isAdult = Age >= 21 && Age2 >=21;
+
+            return isAdult;
         }
     }
 }
