1c9a338ad3dcfb415f11b58994df340c1435f6a9
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-07 14:05:36 -0500

    Tested my DistanceTo method - This method shows the distance between point instance and my DistanceTo(para1, para2) parameters

--- Game_classes/Game_classes/Program.cs
+++ Game_classes/Game_classes/Program.cs
@@ -11,36 +11,12 @@ namespace Game_classes
         static void Main(string[] args)
         {
             Map map = new Map(width: 8, height: 5);    //--map object
-
-            /*  Our map is 8 units wide and 5 units high    
-                let's print the result to the console
-                by calling our OnMap method and pass in our point argument */
+            
             Point point = new Point(x: 8, y: 2);
-            bool results = map.OnMap(point);
-
-            Console.WriteLine("point = {0}", results);
-
-            Program prog = new Program();
             
-            //--Static method
-            Program.Sum_Static(9,1);    //--Can be called in the class directly
-            Sum_Static(10,5);           //--Or not
-
-            //--Non-Static method
-            prog.Sum_Non_Static(20,5);  //--Can ONLY be called onto an instance of a class
+            //--Shows us the distance between points 8,2 -and- 5,5
+            Console.WriteLine(point.DistanceTo(5, 5));  //-- Prints // 4
             
-            
-        }
-        //--Static
-        public static int Sum_Static(int a, int b)
-        {
-            return a + b;
-        }
-
-        //--Non-Static
-        public int Sum_Non_Static(int a, int b)
-        {
-            return a + b;
         }
     }
 
