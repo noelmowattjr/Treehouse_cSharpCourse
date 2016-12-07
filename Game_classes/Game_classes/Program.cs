e72fdec03636e810ec47f7d1ca5cb546a3aa3e1c
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-06 20:06:15 -0500

    Instantiated Point class as point variable - passed it two value - Called my OnMap method and passed point as a parameter

--- Game_classes/Game_classes/Program.cs
+++ Game_classes/Game_classes/Program.cs
@@ -10,50 +10,16 @@ namespace Game_classes
     {
         static void Main(string[] args)
         {
-            Tower tower = new Tower();  //--new tower instance aka new tower object
-
             Map map = new Map(width: 8, height: 5);    //--map object
+
+            /*  Our map is 8 units wide and 5 units high    
+                let's print the result to the console
+                by calling our OnMap method and pass in our point argument */
+            Point point = new Point(x: 8, y: 2);
+            bool results = map.OnMap(point);
+
+            Console.WriteLine("point = {0}", results);
             
-            int area = map.Width * map.Height;
-            Console.WriteLine("The area is: {0}", area);
-
-            //--Testing concept i just learned
-            Console.Write("Enter your age: ");
-            string entry = Console.ReadLine();
-            int age;
-
-            while (!(int.TryParse(entry, out age)))
-            {
-                Console.Write("Invalid! Enter your age: ");
-                entry = Console.ReadLine();
-            }
-
-            Console.Write("Enter your friend's age: ");
-            string entry2 = Console.ReadLine();
-            int age2;
-
-            while (!(int.TryParse(entry2, out age2)))
-            {
-                Console.Write("Invalid! Enter your age: ");
-                entry2 = Console.ReadLine();
-            }
-
-            //--Call function
-            var decision = IsOldEnough(age, age2);
-
-            if (decision == true)
-                Console.WriteLine("You may enter the establishment!");
-
-            if (decision != true)
-                Console.WriteLine("You aren't old enough! Sorry.");
-
-        }
-
-        public static bool IsOldEnough(int Age, int Age2)
-        {
-            bool isAdult = Age >= 21 && Age2 >=21;
-
-            return isAdult;
         }
     }
 }
