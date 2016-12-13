2eb495caa516af4c363a70fad35fa52c9036aca3
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-12 19:38:39 -0500

    Created a class field in Path class and made it private and readonly to prevent users of the class from changing it s value once it has been created

--- Game_classes/Game_classes/Path.cs
+++ Game_classes/Game_classes/Path.cs
@@ -8,9 +8,11 @@ namespace Game_classes
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+
         public Path(MapLocation[] path)
         {
-
+            _path = path;
         }
     }
 }
